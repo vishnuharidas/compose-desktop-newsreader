c490ed2f1462814c186a96b2148765683ee2e988
Vishnu Haridas <hvishnu999@gmail.com>
2022-02-06 03:16:34 +0530

    Update home UI state to use the new data model for articles

--- src/main/kotlin/data/state/HomeUiState.kt
+++ src/main/kotlin/data/state/HomeUiState.kt
@@ -1,7 +1,9 @@
 package data.state
 
+import data.model.Article
+
 data class HomeUiState(
     val isLoading: Boolean = false,
     val error: String? = null,
-    val newsList: List<String>? = null,
+    val newsList: List<Article>? = null,
 )
