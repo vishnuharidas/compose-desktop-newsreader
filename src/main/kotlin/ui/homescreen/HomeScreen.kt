184c699d317b9d2a8589cf90827836ef13cb194c
Vishnu Haridas <hvishnu999@gmail.com>
2022-02-06 01:29:53 +0530

    Home screen now accepts a NewsRepository and initiates a fetch upon launch

--- src/main/kotlin/ui/homescreen/HomeScreen.kt
+++ src/main/kotlin/ui/homescreen/HomeScreen.kt
@@ -1,27 +1,53 @@
 package ui
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import data.NewsRepository
 import ui.homescreen.composable.Headlines
 
 @Composable
 @Preview
-fun HomeScreen() {
+fun HomeScreen(newsRepository: NewsRepository) {
+
+    LaunchedEffect(newsRepository) {
+        newsRepository.fetchNews()
+    }
 
     Row(
         modifier = Modifier.fillMaxSize()
     ) {
+
         // Left headlines
         Column(
             modifier = Modifier
                 .weight(1f)
                 .fillMaxHeight()
         ) {
-            Headlines()
+
+            //Headlines(newsRepository.homeUiState)
+            if (newsRepository.homeUiState.isLoading) {
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xFFEEEEEE)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+
+            } else {
+                Headlines(newsRepository.homeUiState)
+            }
+
         }
 
         // Right - selected news contents
