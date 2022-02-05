11aa74b3572e553adf6df6a7b549c634ce96d6db
Vishnu Haridas <hvishnu999@gmail.com>
2022-02-05 23:46:18 +0530

    Update the main entry point to show the home screen

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,18 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
-@Composable
-@Preview
-fun App() {
-
-    MaterialTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text("Initial commit.")
-        }
-    }
-}
+import ui.HomeScreen
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "Compose Desktop Newsreader"
+        title = "Compose Desktop Newsreader",
     ) {
-        App()
+
+        MaterialTheme {
+            HomeScreen()
+        }
+
     }
 }
