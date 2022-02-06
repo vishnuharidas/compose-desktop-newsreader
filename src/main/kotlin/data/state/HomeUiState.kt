d008789ceac1e75869e69a68c79ce2b8c103d04e
Vishnu Haridas <hvishnu999@gmail.com>
2022-02-07 01:13:12 +0530

    Improve the `HomeUiState` to include API Status, and better listing

--- src/main/kotlin/data/state/HomeUiState.kt
+++ src/main/kotlin/data/state/HomeUiState.kt
@@ -3,7 +3,40 @@ package data.state
 import data.model.Article
 
 data class HomeUiState(
-    val isLoading: Boolean = false,
+    val apiStatus: ApiStatus = ApiStatus.None,
     val error: String? = null,
-    val newsList: List<Article>? = null,
+    val topNews: NewsCollection? = null,
+    val techNews: NewsCollection? = null,
 )
+
+data class NewsCollection(
+    val totalCount: Long = 0,
+    val currentPage: Int = 0,
+    val list: List<Article>? = null,
+)
+
+val NewsCollection.hasMore: Boolean
+    get() {
+        return this.totalCount > 0
+                && !this.list.isNullOrEmpty()
+                && this.totalCount != this.list.size.toLong()
+    }
+
+fun NewsCollection?.newOrAdd(
+    totalCount: Long,
+    list: List<Article>?
+): NewsCollection {
+
+    if (this == null) return NewsCollection(
+        totalCount = totalCount,
+        currentPage = 1,
+        list = list
+    )
+
+    if (list.isNullOrEmpty()) return this
+
+    return this.copy(
+        currentPage = this.currentPage + 1,
+        list = this.list?.plus(list)
+    )
+}
