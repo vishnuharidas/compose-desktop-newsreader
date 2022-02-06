5745aa4aca461f7016f30afbc59106208d3cc666
Vishnu Haridas <hvishnu999@gmail.com>
2022-02-07 01:16:30 +0530

    `Headlines` composable now shows errors, load more indicator, and load more error.

--- src/main/kotlin/ui/homescreen/composable/Headlines.kt
+++ src/main/kotlin/ui/homescreen/composable/Headlines.kt
@@ -7,29 +7,65 @@ import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import data.state.ApiStatus
 import data.state.HomeUiState
+import data.state.hasMore
 import kotlinx.coroutines.runBlocking
 
 @Composable
-fun Headlines(state: HomeUiState) {
+fun Headlines(
+    state: HomeUiState,
+    onLoadMore: () -> Unit,
+) {
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
     ) {
 
-        if (state.newsList.isNullOrEmpty()) {
+        if (state.apiStatus is ApiStatus.Failure) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFFEEDDDD))
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    state.apiStatus.message ?: "Some unknown error occurred. Please try again later.",
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(
+                        color = Color(0xFFEE5555),
+                        fontSize = 16.sp,
+                    )
+                )
+
+                Button(
+                    onClick = {
+                        //TODO make an actual call
+                    },
+                ) {
+                    Text("Retry")
+                }
+            }
+
+        } else if (state.topNews == null || state.topNews.list.isNullOrEmpty()) {
 
             Column(
                 modifier = Modifier
@@ -57,24 +93,81 @@ fun Headlines(state: HomeUiState) {
                 }
             }
 
-
         } else {
-            // Using a state to enable drag-to-scroll (like mobile devices) on desktop applications.
-            // Ref: https://github.com/JetBrains/compose-jb/issues/1555#issuecomment-987958210
-            val scrollState = rememberLazyListState()
 
-            LazyColumn(
-                state = scrollState,
-                modifier = Modifier
-                    .draggable(rememberDraggableState {
-                        runBlocking {
-                            scrollState.scrollBy(-it)
-                        }
-                    }, orientation = Orientation.Vertical),
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.BottomCenter
             ) {
 
-                items(state.newsList) {
-                    NewsCard(it)
+                // Using a state to enable drag-to-scroll (like mobile devices) on desktop applications.
+                // Ref: https://github.com/JetBrains/compose-jb/issues/1555#issuecomment-987958210
+                val scrollState = rememberLazyListState()
+
+                LazyColumn(
+                    state = scrollState,
+                    modifier = Modifier
+                        .draggable(rememberDraggableState {
+                            runBlocking {
+                                scrollState.scrollBy(-it)
+                            }
+                        }, orientation = Orientation.Vertical),
+                ) {
+
+                    itemsIndexed(state.topNews.list) { index, item ->
+
+                        NewsCard(item)
+
+                        // Load more if there are more items to load.
+                        if (index == state.topNews.list.size - 1 // Reached the last item
+                            && state.apiStatus !is ApiStatus.LoadMoreFailure // Don't try loading more if there was a failure when loading more.
+                            && state.topNews.hasMore
+                        ) {
+                            onLoadMore()
+                        }
+                    }
+
+                }
+
+                if (state.apiStatus == ApiStatus.LoadingMore) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.White)
+                            .padding(8.dp)
+                            .alpha(0.9f),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+
+                if (state.apiStatus is ApiStatus.LoadMoreFailure) {
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color(0xFFEEDDDD))
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            state.apiStatus.message ?: "Some unknown error occurred. Please try again later.",
+                            textAlign = TextAlign.Center,
+                            style = TextStyle(
+                                color = Color(0xFFEE5555),
+                                fontSize = 12.sp,
+                            )
+                        )
+
+                        Button(
+                            onClick = onLoadMore,
+                        ) {
+                            Text("Try again")
+                        }
+                    }
+
                 }
 
             }
