8fe1233b07050e6113ac91dd57d7c7316db1442f
Vishnu Haridas <hvishnu999@gmail.com>
2022-02-15 13:19:32 +0530

    Update home screen to show the details of the selected article

--- src/main/kotlin/ui/homescreen/HomeScreen.kt
+++ src/main/kotlin/ui/homescreen/HomeScreen.kt
@@ -3,16 +3,19 @@ package ui.homescreen
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import data.NewsRepository
+import data.model.Article
+import ui.homescreen.composable.Contents
 import ui.homescreen.composable.Headlines
 
 @Composable
 @Preview
 fun HomeScreen(newsRepository: NewsRepository) {
+
+    var selectedArticle by remember { mutableStateOf<Article?>(null) }
 
     LaunchedEffect(newsRepository) {
         newsRepository.fetchNews()
@@ -34,6 +37,9 @@ fun HomeScreen(newsRepository: NewsRepository) {
                 onLoadMore = { newsRepository.fetchMoreNews() },
                 onRetry = {
                     newsRepository.fetchAgain()
+                },
+                onClick = {
+                    selectedArticle = it
                 }
             )
 
@@ -45,11 +51,23 @@ fun HomeScreen(newsRepository: NewsRepository) {
                 .weight(3f)
                 .fillMaxHeight()
         ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text("Contents will appear here.")
+
+            val article = selectedArticle
+            if (article != null) {
+
+                Contents(
+                    article = article
+                )
+
+            } else {
+
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("Select a news from the list to read")
+                }
+
             }
         }
     }
