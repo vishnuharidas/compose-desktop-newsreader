73a2770c074c1fbfb9703a605eb45a06b18ba74b
Vishnu Haridas <hvishnu999@gmail.com>
2022-02-16 22:27:21 +0530

    Add fonts Roboto Slab regular and light, add supporting resource file

---
+++ src/main/kotlin/resources/Fonts.kt
@@ -0,0 +1,23 @@
+package resources
+
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
+
+object AppFonts {
+
+    val RobotoSlabLight = FontFamily(
+        Font(
+            "fonts/RobotoSlab-Light.ttf",
+            weight = FontWeight.Light
+        )
+    )
+
+    val RobotoSlabRegular = FontFamily(
+        Font(
+            "fonts/RobotoSlab-Regular.ttf",
+            weight = FontWeight.Light
+        )
+    )
+
+}
