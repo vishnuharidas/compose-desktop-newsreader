0a46d377f83df200b82f2c14df323b1b6f9842e5
Vishnu Haridas <hvishnu999@gmail.com>
2022-02-22 23:59:59 +0530

    Add key event handler in the main window to capture up/down arrow keys to navigate through the list of news

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.iamvishnu.compose.Compose_Desktop_Newsreader.BuildConfig
@@ -7,6 +12,7 @@ import data.NewsRepository
 import data.network.NewsApi
 import ui.homescreen.HomeScreen
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() = application {
 
     val newsRepository = NewsRepository()
@@ -16,6 +22,21 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Compose Desktop Newsreader",
+        onKeyEvent = {
+
+            when {
+                it.key == Key.DirectionDown && it.type == KeyEventType.KeyUp -> {
+                    newsRepository.next()
+                    true
+                }
+                it.key == Key.DirectionUp && it.type == KeyEventType.KeyUp -> {
+                    newsRepository.prev()
+                    true
+                }
+                else -> false
+            }
+
+        }
     ) {
 
         MaterialTheme {
